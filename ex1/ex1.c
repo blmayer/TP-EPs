14d67f2f68f16fbb497e390779007ea1b00e41e2
blmayer <bleemayer@gmail.com>
2018-01-18 03:09:08 +0000

    Created more functions

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -15,65 +15,66 @@ typedef struct dllist dllist;
 void insert(int new, dllist *lst) {
 
     // Create the new element
+    dllist *cur;
+    dllist *temp = malloc(sizeof(dllist));
+    temp -> val = new;
+    cur = lst;
+
+    // Loop in our list to find the correct place to insert the new element
+    while(cur -> val < new) {
+        cur = cur -> next;
+    }
+    printf("current val is %d\n", cur -> val);
+    
+    temp -> val = cur -> val;
+    cur -> val = new;
+
+    temp -> next = cur;
+    temp -> prev = cur -> prev;
+}
+
+void drop(int del, dllist *lst) {
+
+    // Create the new element
     dllist *el;
     dllist *temp = malloc(sizeof(dllist));
     el = lst;
-    temp -> val = new;
 
     // Loop in our list to find the correct place to insert the new element
-    while(el -> next != NULL && el -> val < new) {
-        printf("val is %d\n", el -> val);
+    while(el -> next != NULL && el -> val != del) {
         el = el -> next;
     }
     printf("el val is %d\n", el -> val);
+}
 
-    if(el -> val < new) {
-        puts("after last");
-        el -> next = temp;
-        temp -> prev = el;
-        temp -> next = NULL;
-    } else {
-        puts("before last");
-        temp -> prev = el;
-        temp -> next = el -> next;
-        temp -> val = el -> val;
-        el -> next = temp;
-        el -> val = new;
-    } 
+void print_list(dllist *lst) {
+
+    dllist *cur = lst;
+    printf("%d\n", cur -> val);
+
+    if(cur -> next != NULL) {
+        print_list(cur -> next);
+    }
 }
 
 void main(void) {
+
     dllist *ages = malloc(sizeof(dllist));
-    dllist *ages1 = malloc(sizeof(dllist));
-    dllist *ages2 = malloc(sizeof(dllist));
-    dllist *ages3 = malloc(sizeof(dllist));
-
-    ages -> next = ages1;
-    ages1 -> next = ages2;
-    ages2 -> next = ages3;
-    ages3 -> next = NULL;
-
-    ages -> prev = NULL;
-    ages1 -> prev = ages;
-    ages2 -> prev = ages1;
-    ages3 -> prev = ages2;
-
     ages -> val = 2;
-    ages1 -> val = 3;
-    ages2 -> val = 8;
-    ages3 -> val = 10;
+    ages -> prev = NULL;
+    ages -> next = NULL;
 
-    // Insert new element
-    insert(11, ages);
+    // Insert new elements
+    insert(4, ages);
+    // insert(10, ages);
+    // insert(3, ages);
+    // insert(7, ages);
+    // insert(12, ages);
+    // insert(4, ages);
 
     puts("Inserted.");
 
     // Printing values
-    printf("first %d\n", ages -> val);
-    printf("second %d\n", ages -> next -> val);
-    printf("third %d\n", ages -> next -> next -> val);
-    printf("fourth %d\n", ages -> next -> next -> next -> val);
-    printf("fifth %d\n", ages -> next -> next -> next -> next -> val);
-
+    print_list(ages);
 }
 
